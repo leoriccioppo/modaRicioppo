5f3588e2140dfd85bcecff39b0cd006c6ebc2a26
Leonora Riccioppo <119987608+leoriccioppo@users.noreply.github.com>
2023-10-21 14:27:00 -0300

    Criacao das pages e organizacao rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Header } from './components/header/index.jsx'
 import { ItemListContainer } from './components/itemListContainer/index.jsx'
+import { Input } from './components/input/index.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,9 +9,10 @@ function App() {
   return (
     <>
       <Header/>
+      <Input/>
       <ItemListContainer/>
     </>
   )
 }
 
-export default App
+export {App}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,8 @@
+const Home = () =>{
+
+    return(
+        <h1>here bitch!</h1>
+    )
+}
+
+export {Home}
