e51b15c8367458532b56c77b097feb277726659d
Leonora Riccioppo <119987608+leoriccioppo@users.noreply.github.com>
2023-11-05 00:17:00 -0300

    refactor style

--- src/components/counter/ItemCount.jsx
+++ src/components/counter/ItemCount.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { PiCaretUpBold } from 'react-icons/pi';
-import { PiCaretDownBold } from 'react-icons/pi';
 import { useCart } from '../../contexts/cartContext.jsx';
 import { useProducts } from '../../contexts/fakeStoreApiContext.jsx';
+import IconButton from '@mui/material/IconButton';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { Box, Typography } from '@mui/material';
 
-const ItemCount = ({ stock, itemId}) => {
+const ItemCount = ({ stock}) => {
   const [quantity, setQuantity] = useState(0); // Inicializa count com 0 usando useState
 
   const { onAddToCart} = useCart(); // Obtém a função onAddToCart do contexto de carrinho
@@ -23,23 +25,19 @@ const ItemCount = ({ stock, itemId}) => {
     }
   };
 
-  const handleAddToCart = () => {
-    const itemDetails = products.find(product => product.id === itemId);
-
-    if (quantity > 0) {
-      onAddToCart(itemDetails, quantity);
-      setQuantity(0);
-    }
-    
-  };
 
   return (
-    <div>
-      <div className='counterUp' onClick={handleIncrement}><PiCaretUpBold/></div>
-      <div className='QuantityItems'>{quantity}</div>
-      <div className='counterDown' onClick={handleDecrement}><PiCaretDownBold/></div>
-      <button onClick={handleAddToCart}>Add to shop bag</button>
-    </div>
+    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+      <IconButton style={{ padding: '0' }} onClick={handleIncrement} color="primary" aria-label="Arrow Up">
+      <ArrowDropUpIcon />
+    </IconButton>
+
+    <Typography variant="p" component="div">{quantity}</Typography>
+
+    <IconButton style={{ padding: '0' }} onClick={handleDecrement} color="primary" aria-label="Arrow Down">
+      <ArrowDropDownIcon />
+    </IconButton>
+    </Box>
   );
 };
 
